2e1114107b64e9acf179a362ea353657412cece7
O-liverr <oliekoech@gmail.com>
2025-06-24 03:57:25 +0300

    models

---
+++ server/models/episode.py
@@ -0,0 +1,8 @@
+from . import db
+
+class Episode(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String, nullable=False)
+    number = db.Column(db.Integer, nullable=False)
+
+    appearances = db.relationship("Appearance", back_populates="episode", cascade="all, delete-orphan")
