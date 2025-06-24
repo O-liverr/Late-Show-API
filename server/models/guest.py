3a2e0df0a7f402c4bad3d2d1bd4638f6f2a528d4
O-liverr <oliekoech@gmail.com>
2025-06-24 03:55:42 +0300

    models

---
+++ server/models/guest.py
@@ -0,0 +1,8 @@
+from . import db
+
+class Guest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    occupation = db.Column(db.String, nullable=False)
+
+    appearances = db.relationship("Appearance", back_populates="guest", cascade="all, delete-orphan")
